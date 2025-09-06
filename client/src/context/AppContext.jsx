9f3f0d000a2c5e3bf5650fddcd92e291d171165c
Malay146 <pmalay694@gmail.com>
2025-09-07 01:40:02 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ export const AppContent = createContext()
 
 export const AppContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://codecrew-mindsprint-backend.onrender.com'
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [userData, setUserData] = useState(null)
 
